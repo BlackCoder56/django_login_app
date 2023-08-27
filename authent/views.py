36d98e58246f8d95f91edf26776366728b763cad
Elly <blackcoda56@gmail.com>
2023-08-27 04:13:55 +0300

    version 1

--- authent/views.py
+++ authent/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
